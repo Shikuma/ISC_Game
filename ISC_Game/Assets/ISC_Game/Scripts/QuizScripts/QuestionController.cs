78a2beade9662fb6ebd06cbdb515ab05ed003414
maxman43 <mburchell@unomaha.edu>
2017-03-14 15:54:12 -0500

    Question Logic Started
    - Reads from text file to get questions
    - currently has a dummy file (Test.txt)
    - Clicking on answer buttons changes which line from the text file is
    displayed

---
+++ ISC_Game/Assets/ISC_Game/Scripts/QuizScripts/QuestionController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+
+public class QuestionController : MonoBehaviour {
+
+	public Text questionBox;
+	private int qNum = 1;
+
+	//public TextAsset qSource;
+	protected FileInfo theSourceFile = null;
+	protected StreamReader reader = null;
+	protected string text = " ";
+
+	private List<string> qList = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		theSourceFile = new FileInfo (Application.dataPath + "/ISC_Game/Scripts/QuizScripts/Test.txt");
+		reader = theSourceFile.OpenText();
+
+		while (true) {
+			text = reader.ReadLine();
+			//Console.WriteLine(text);
+			if (text == null) {
+				break;
+			}
+			qList.Add(text);
+		}
+
+		reader.Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (qNum <= qList.Count) {
+			questionBox.text = qList [qNum - 1];
+		}
+			
+	}
+
+	public void SelectAnswer(int answer){
+		qNum = answer;
+	}
+}
