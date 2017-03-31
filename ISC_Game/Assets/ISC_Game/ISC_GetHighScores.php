9f22f7ac1d654cbc60562c6b96c2e2bdda903e41
Max Schuessler <Max Schuessler>
2017-03-30 19:14:10 -0500

    Not really any changes, just making sure it's pushed

--- ISC_Game/Assets/ISC_Game/ISC_GetHighScores.php
+++ ISC_Game/Assets/ISC_Game/ISC_GetHighScores.php
@@ -12,7 +12,7 @@
 
     $sql = "SELECT id, score, user_id, date_complete, first_name, last_name FROM games";
     $result = mysqli_query($conn, $sql);
-
+    
     if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_assoc($result)){
             echo("GameID:".$row['id']."|score:".$row['score']."|user_id:".$row['user_id']."|Date_complete:".$row[date_complete]."|first_name:".$row[first_name]."|last_name:".$row[last_name].";");
