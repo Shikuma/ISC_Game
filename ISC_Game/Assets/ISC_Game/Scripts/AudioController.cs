25b901d7c0e4e4b77159e58f0f6bb18b008efbe8
maxman43 <mburchell@unomaha.edu>
2017-04-05 16:52:29 -0500

    Audio stuff added. Sprite added. Not all audio implemented. Not implemented sprites.

---
+++ ISC_Game/Assets/ISC_Game/Scripts/AudioController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : Singleton<AudioController> {
+
+	protected AudioController () {}
+
+	public List<AudioClip> sfx;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(transform.gameObject);
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void playSFX(int index){
+		audioSource.PlayOneShot (sfx [index]);
+	}
+}
