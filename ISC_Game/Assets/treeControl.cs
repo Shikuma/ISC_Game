5c8562ed05b24f87b18aaae813a7ced78b4ae21c
maxman43 <mburchell@unomaha.edu>
2017-04-25 12:11:53 -0500

    moving background trees

---
+++ ISC_Game/Assets/treeControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treeControl : MonoBehaviour {
+
+	public float speed = 5;
+	bool moving = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving)
+			transform.Translate(-speed * Time.deltaTime, 0, 0);
+
+		if (transform.position.x < -2.0f) {
+			transform.position = new Vector2(22.0f, transform.position.y);
+			nextTree (Random.Range (0.1f, 5f));
+		}
+	}
+
+	IEnumerator nextTree(float sec){
+		moving = false;
+		yield return new WaitForSeconds (sec);
+		moving = true;
+	}
+}
