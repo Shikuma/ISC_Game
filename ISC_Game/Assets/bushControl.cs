a7b5af5ccf3b6613e957196cd55b9a29cade91ce
maxman43 <mburchell@unomaha.edu>
2017-04-27 16:04:14 -0500

    bush sprite in

---
+++ ISC_Game/Assets/bushControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bushControl : MonoBehaviour {
+	public GameObject bush;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (bush, transform.position, Quaternion.identity).transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
